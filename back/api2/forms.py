f485ad62975a47482458c45bd7d1b2a87c972541
Italo Oliveira <38302009+arkanttus@users.noreply.github.com>
2020-05-26 22:59:17 -0300

    update: number of cols and rows textarea

--- back/api2/forms.py
+++ back/api2/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class ValoresForm(forms.Form):
-    conteudo = forms.CharField(label="Tabela de Casos", widget=forms.Textarea, max_length=800)
-    conteudo2 = forms.CharField(label="Tabela de Detalhes", widget=forms.Textarea, max_length=800)
+    conteudo = forms.CharField(label="Tabela de Casos", widget=forms.Textarea(attrs={'rows': 20, 'cols': 100}), max_length=800)
+    conteudo2 = forms.CharField(label="Tabela de Detalhes", widget=forms.Textarea(attrs={'rows': 20, 'cols': 100}), max_length=800)
